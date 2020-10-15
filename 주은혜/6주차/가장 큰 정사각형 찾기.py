6c9d9442e6e487f5d48f06c3d6366b80a5421e47
Eunhye-Joo <65636174+Eunhye-Joo@users.noreply.github.com>
2020-10-15 10:53:06 +0900

    Update 가장 큰 정사각형 찾기.py

--- 주은혜/6주차/가장 큰 정사각형 찾기.py
+++ 주은혜/6주차/가장 큰 정사각형 찾기.py
@@ -1,33 +1,34 @@
+
 ##가장 큰 정사각형 찾기##
+# 문제 링크 https://programmers.co.kr/learn/courses/30/lessons/12905
+# 세부 설명 https://codedrive.tistory.com/53
 
 # board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
 board = [[1,0],[0,0]]
-print(board)
+# print(sum(board[0][:]))
+# print(sum(board[:][0]))
 
-tmp = 0
-for i in range(len(board)):
-    tmp += sum(board[i][:])
-print(tmp)
-if board[0][0] == 1 and tmp == 1:
+if board[0][0] == 1 and sum(board[0][:]) == 1 and sum(board[:][0]):
     answer = 1
 
 else:
 
     max_point = 0
     for i in range(1,len(board)):
-        for j in range(1, len(board[0])):
+        for j in range(1,len(board[0])):
+
             if board[i][j] == 0:
                 continue
             else:
-                min_point = min(board[i][j - 1], board[i - 1][j], board[i - 1][j - 1]) + 1
-                board[i][j] = min_point
+                board[i][j] = min(board[i-1][j],board[i][j-1],board[i-1][j-1])+1
 
-            if max_point < board[i][j]:
-                max_point = board[i][j]
+                if max_point < board[i][j]:
+                    max_point = board[i][j]
 
-    if max_point == 0:
-        answer = 0
-    else:
-        answer = max_point*max_point
+            if max_point == 0:
+                answer = 0
+
+            else:
+                answer = max_point*max_point
 
 print(answer)
